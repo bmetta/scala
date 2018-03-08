8798ca8817a21bae08d24c064c7b3abb66f898cd
bmetta <bhogeswar.m@gmail.com>
2018-03-08 15:39:21 +0530

    Array methods examples

--- collections/Array.scala
+++ collections/Array.scala
@@ -1,6 +1,9 @@
 /**
  *  Arrays are mutable, indexed collections of values. Array[T] is Scala's
  *  representation for Java's T[].
+ *
+ * constructor
+ *  new Array(_length: Int)
  */
 object Array extends App {
   val a = Array(1, 2, 3, 4, 5)
@@ -15,12 +18,16 @@ object Array extends App {
   val aStr = a.addString(b) // 12345
   println(aStr)
 
-  // def addString(b: StringBuilder, sep: String): StringBuilder
+  /**
+   * def addString(b: StringBuilder, sep: String): StringBuilder
+   */
   val b1 = new StringBuilder
   val aStr1 = a.addString(b1, ", ") // 1, 2, 3, 4, 5
   println(aStr1)
 
-  // def addString(b: StringBuilder, start: String, sep: String, end: String): StringBuilder
+  /**
+   * def addString(b: StringBuilder, start: String, sep: String, end: String): StringBuilder
+   */
   val b2 = new StringBuilder
   val aStr2 = a.addString(b2, "Array(", ", ", ")") // 1, 2, 3, 4, 5
   println(aStr2)
@@ -64,11 +71,234 @@ object Array extends App {
    * def combinations(n: Int): collection.Iterator[Array[T]]
    */
   val str = "abbbc"
-  str.combinations(2).foreach(println)
+  val combs = str.combinations(2) // Array(ab, ac, bb, bc)
+  println(combs.mkString(", "))
 
   /**
    * def contains[A1 >: A](elem: A1): Boolean
    */
   println(a.contains(5)) // true
   println(a.contains(6)) // false
+
+  /**
+   * def containsSlice[B](that: GenSeq[B]): Boolean
+   *
+   * Tests whether this mutable indexed sequence contains a given sequence
+   * as a slice.
+   */
+  println(a.containsSlice(Array(1, 2, 3))) // true
+  println(a.containsSlice(Array(1, 2, 4))) // false
+
+  /**
+   * def copyToArray(xs: Array[A], start: Int, len: Int): Unit
+   * def copyToArray(xs: Array[A], start: Int): Unit
+   * def copyToArray(xs: Array[A]): Unit
+   */
+  val a1 = new Array[Int](6)
+  a.copyToArray(a1, 2, 3) // 0 0 1 2 3 0
+  println(a1.mkString(", "))
+
+  /**
+   * def copyToBuffer[B >: A](dest: Buffer[B]): Unit
+   */
+  val buff = new collection.mutable.ArrayBuffer[Int]()
+  a.copyToBuffer(buff)
+  println(buff.mkString(", "))
+
+  /**
+   * def corresponds[B](that: GenSeq[B])(p: (T, B) => Boolean): Boolean
+   */
+
+
+  /**
+   * def count(p: (T) => Boolean): Int
+   *
+   * Counts the number of elements in the mutable indexed sequence which
+   * satisfy a predicate.
+   */
+  println("even elements count: " + a.count(_ % 2 == 0))
+  println("odd  elements count: " + a.count(x => x % 2 != 0))
+
+  /**
+   * def deep: collection.IndexedSeq[Any]
+   */
+  val deepString = Array(Array(1, 2), Array(3, 4)).deep.toString
+  println(deepString)
+
+  /**
+   * def diff(that: collection.Seq[T]): Array[T]
+   * 
+   * Computes the multiset difference between this array and another
+   * sequence.
+   */
+  val low = Array(1, 2, 3, 4, 5)
+  val med = Array(      3, 4, 5, 6, 7)
+  val lmDiff = low.diff(med) // Array(1, 2)
+  val mlDiff = med.diff(low) // Array(6, 7)
+  println(lmDiff.mkString(", "))
+  println(mlDiff.mkString(", "))
+
+  /**
+   * def distinct: Array[T]
+   *
+   * Builds a new mutable indexed sequence from this mutable indexed
+   * sequence without any duplicate elements.
+   */
+  val d1 = Array(1, 2, 1, 3, 2, 3, 4, 5, 5)
+  val d1Dist = d1.distinct // Array(1, 2, 3, 4, 5)
+  println(d1Dist.mkString(", "))
+
+  /**
+   * def drop(n: Int): Array[T]
+   * Selects all elements except first n ones.
+   * 
+   * def dropRight(n: Int): Array[T]
+   * Selects all elements except last n ones.
+   *
+   * def dropWhile(p: (T) ⇒ Boolean): Array[T]
+   * Drops longest prefix of elements that satisfy a predicate.
+   */
+
+  /**
+   * def filter(p: (T) ⇒ Boolean): Array[T]
+   * Selects all elements of this mutable indexed sequence which satisfy
+   * a predicate.
+   */
+  val f = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val fEven = f.filter(_ % 2 == 0) // Array(2, 4, 6, 8, 10)
+  println(fEven.mkString(", "))
+
+  /**
+   * def flatten[U](implicit asTrav: (T) => collection.Traversable[U],
+   *                m: ClassTag[U]): Array[U]
+   *
+   * Flattens a two-dimensional array by concatenating all its rows into
+   * a single array.
+   */
+  val aoa = Array(Array(1, 2), Array(3, 4))
+  val aoaToArray = aoa.flatten // Array(1, 2, 3, 4)
+  println(aoaToArray.mkString(", "))
+
+  val aos = Array("Hello", "World")
+  val aoc = aos.flatten // Array[Char](H, e, l, l, o, W, o, r, l, d)
+  println(aoc.mkString(", "))
+
+  val optionVals = Array(Some(1), None, Some(3), None)
+  val intVals = optionVals.flatten // Array[Int](1, 3)
+  println(intVals.mkString(", "))
+
+  /**
+   * def map[B](f: (A) => B): Array[B]
+   *
+   * Builds a new collection by applying a function to all elements of
+   * this array
+   */
+  val fruits = Array("apple", "banana", "orange")
+  val fruitsInCaps = fruits.map(_.toUpperCase) // Array[String]("APPLE", "BANANA", "ORANGE")
+  println(fruitsInCaps.mkString(", "))
+
+  /**
+   * def flatMap[B](f: (A) => GenTraversableOnce[B]): Array[B]
+   */
+  val fruitsInChars = fruits.flatMap(_.toUpperCase) // Array[Char](A,P,P,L,E,B,A,N,A,N,A,O,R,A,N,G,E)
+  println(fruitsInChars.mkString(", "))
+
+  // Option/Some/None classes
+  def toInt(s: String): Option[Int] = {
+    try {
+      Some(Integer.parseInt(s.trim))
+    } catch {
+      case e: Exception => None
+    }
+  }
+  val strings = Array("1", "2", "foo", "3", "bar")
+  val stringsMap = strings.map(toInt) // Array(Some(1), Some(2), None, Some(3), None)
+  val stringsFlatMap = strings.flatMap(toInt) // Array(1, 2, 3)
+  println(stringsMap.mkString(", "))
+  println(stringsFlatMap.mkString(", "))
+  println(stringsFlatMap.sum) // 6
+
+  /**
+   * def groupBy[K](f: (T) ⇒ K): Map[K, Array[T]]
+   *
+   * Partitions this mutable indexed sequence into a map of mutable indexed
+   * sequences according to some discriminator function
+   */
+  val groups = (1 to 20).toArray groupBy {
+    case i if (i < 5)  => "g1"
+    case i if (i < 10) => "g2"
+    case i if (i < 15) => "g3"
+    case _ => "g4"
+  }
+  // groups: scala.collection.immutable.Map[String,Array[Int]] =
+  //    Map(g3 -> Array(10, 11, 12, 13, 14), 
+  //        g2 -> Array(5, 6, 7, 8, 9), 
+  //        g1 -> Array(1, 2, 3, 4), 
+  //        g4 -> Array(15, 16, 17, 18, 19, 20))
+  groups.foreach(group => println(group._1 + " => " + group._2.mkString(", ")))
+  
+  val mods = (1 to 20).toArray groupBy(_ % 4)
+  mods.foreach(mod => println(mod._1 + " => " + mod._2.mkString(", ")))
+
+  /**
+   * def partition(p: (T) ⇒ Boolean): (Array[T], Array[T])
+   * Partitions this mutable indexed sequence in two mutable indexed
+   * sequences according to a predicate.
+   */
+  val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  // numbers: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val parts = numbers.partition(_ % 2 == 0)
+  // parts: (Array[Int], Array[Int]) = (Array(2, 4, 6, 8, 10),Array(1, 3, 5, 7, 9))
+  println(parts._1.mkString(", "))
+  println(parts._2.mkString(", "))
+
+  /**
+   * def permutations: collection.Iterator[Array[T]]
+   *
+   * Iterates over distinct permutations.
+   */
+  val nums = Array(1, 2, 3)
+  val perms = nums.permutations.toArray
+  // perms: Array[Array[Int]] = Array(Array(1, 2, 3), Array(1, 3, 2), Array(2, 1, 3),
+  //                                  Array(2, 3, 1), Array(3, 1, 2), Array(3, 2, 1))
+  val permsMap = perms.map(arr => arr.mkString)
+  // permsMap: Array[String] = Array(123, 132, 213, 231, 312, 321)
+  println(permsMap.mkString(", "))
+
+  /**
+   * def reduce[A1 >: A](op: (A1, A1) => A1): A1
+   *  
+   * Reduces the elements of this mutable indexed sequence using the specified
+   * associative binary operator.
+   */
+  val aoChars = Array("A", "B", "C")
+  // aoChars: Array[String] = Array(A, B, C)
+  println(aoChars.reduce(_ + _)) // "ABC"
+  
+  val aoNums = Array(12, 6, 15, 2, 20, 9)
+  println(aoNums.reduce(_ min _)) // 2
+  println(aoNums.reduce(_ max _)) // 20
+  println(aoNums.reduce(_ + _)) // 64
+
+  val peeps = Vector("al", "hannah", "emily", "christina", "aleka")
+  val peepsMax = peeps.reduce((x, y) => if(x.length > y.length) x else y)
+  println(peepsMax)
+
+  /**
+   * def reduceLeft[B >: A](op: (B, T) => B): B
+   * Applies a binary operator to all elements of this mutable indexed sequence,
+   * going left to right.
+   *
+   * def reduceRight[B >: A](op: (T, B) => B): B
+   * Applies a binary operator to all elements of this mutable indexed sequence,
+   * going right to left.
+   */
+  // calculate min value from collection
+  val aoStrs = Array("one", "two", "three")
+  val leftMin = aoStrs.reduceLeft((result, element) =>
+                    if (element.length < result.length) element else result)
+  println(leftMin)
+  val rightMin = aoStrs.reduceRight((element, result) =>
+                    if (element.length < result.length) element else result)
+  println(rightMin)
 }
