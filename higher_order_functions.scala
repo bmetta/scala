64921dd9189c6128970e1b4f57813180c99f9210
bmetta <bhogeswar.m@gmail.com>
2018-03-12 19:05:24 +0530

    modified

---
+++ higher_order_functions.scala
@@ -0,0 +1,53 @@
+object HigherOrderFunctions extends App {
+  val salaries = Seq(30000, 70000, 40000)
+  // doubleSalary is a function which takes a single Int, sal, and
+  // returns sal * 2
+  // (parameter list) => returned
+  val doubleSalary = (sal: Int) => sal * 2
+  val newSalaries = salaries map (doubleSalary)
+  println(newSalaries)
+
+  // To shrink the code, we could make the function anonymous and pass
+  // it directly as an argument to map
+  val newSalaries2 = salaries map (sal => sal * 2)
+  println(newSalaries2)
+
+  // idiomatic way to write the same piece of code would be
+  val newSalaries3 = salaries map (_ * 2)
+  println(newSalaries3)
+
+
+  /**
+   * Functions that accept functions
+   * HigherOrderFunctions is used to reduce redundant code.
+   */
+  object SalaryRaiser {
+    def smallPromotion(salaries: List[Double]): List[Double] = {
+      salaries.map(salary => salary * 1.1)
+    }
+    def greatPromotion(salaries: List[Double]): List[Double] = {
+      salaries.map(salary => salary * math.log(salary))
+    }
+    def hugePromotion(salaries: List[Double]): List[Double] = {
+      salaries.map(salary => salary * salary)
+    }
+  }
+
+  // Extract the repeated code into a HigherOrderFunction like
+  object SalaryRaiser1 {
+    def promotion(salaries: List[Double], promotionFunction: Double => Double):
+       List[Double] = {
+      salaries.map(promotionFunction)
+    }
+
+    def smallPromotion(salaries: List[Double]): List[Double] = {
+      promotion(salaries, salary => salary * 1.1)
+    }
+    def greatPromotion(salaries: List[Double]): List[Double] = {
+      promotion(salaries, salary => salary * math.log(salary))
+    }
+    def hugePromotion(salaries: List[Double]): List[Double] = {
+      promotion(salaries, salary => salary * salary)
+    }
+  }
+}
--- scala-cookbook_programs/9.Functional_Programming/9.8.partial_functions.scala
+++ scala-cookbook_programs/9.Functional_Programming/9.8.partial_functions.scala
@@ -1,2 +1,55 @@
-object PartialFunctions extends App {
+/**
+ * trait PartialFunction[-A, +B] extends (A) => B
+ * Partial function of type PartialFunction[A, B] is a unary function
+ * (A) => B can be interpreted as a function that transforms a type A
+ * into a resulting type B.
+ */
+object PartialFunction extends App {
+  /**
+   * Normal function that divides one number by another
+   */
+  //val divide = (x: Int) => 42 / x
+  //divide(0) // java.lang.ArithmeticException: / by zero
+
+  /**
+   * using PartialFunction
+   */
+  val divide = new PartialFunction[Int, Int] {
+    def apply(x: Int) = 42 / x
+    def isDefinedAt(x: Int) = x != 0
+  }
+  if (divide.isDefinedAt(1)) // true
+    println(divide(1)) // 42
+  if (divide.isDefinedAt(0)) // false
+    println(divide(0))
+
+  /**
+   *  Partial functions written with case statement
+   *  This doesnt explicitly implement the isDefinedAt method,
+   *  it works exactly same as the previous divide function
+   */
+  val divide2: PartialFunction[Int, Int] = {
+    case d: Int if d != 0 => 42 / d
+  }
+  if (divide2.isDefinedAt(1)) // true
+    println(divide2(1)) // 42
+  if (divide2.isDefinedAt(0)) // false
+    println(divide2(0))
+
+  val sample = 1 to 10
+  val isEven: PartialFunction[Int, String] = {
+    case x if x % 2 == 0 => x + " is even"
+  }
+  val isOdd: PartialFunction[Int, String] = {
+    case x if x % 2 != 0 => x + " is odd"
+  }
+  val numbers = sample map (isEven orElse isOdd)
+  println(numbers)
+
+  // collect method is written to test the isDefinedAt method for
+  // each element it’s given
+  val oddNumbers = sample collect (isOdd)
+  println(oddNumbers)
+  val evenNumbers = sample collect (isEven)
+  println(evenNumbers)
 }
