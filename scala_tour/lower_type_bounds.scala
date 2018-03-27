88e20cf7c40e38e803f1c80f0585a8a93f8c3cc5
bmetta <bhogeswar.m@gmail.com>
2018-03-27 18:08:48 +0530

    linked list example for lower_type_bounds

--- scala_tour/lower_type_bounds.scala
+++ scala_tour/lower_type_bounds.scala
@@ -14,5 +14,46 @@
  *  ListNode is a node which contains an element of type B
  *    and a reference to the rest of the list (tail)
  */
-object LowerTypeBounds extends App {
+trait Node[+B] {
+  def prepend[U >: B](element: U): Node[U]
+  //def prepend[B](element: B): Node[B]
+  def foreach(f: (B) => Unit): Unit = {
+    this match {
+      case ListNode(head, tail) => {
+        f(head)
+        tail.foreach(f)
+      }
+      case Nil() => {}
+    }
+  }
+}
+case class ListNode[+B](h: B, t: Node[B]) extends Node[B] {
+  def prepend[U >: B](element: U): ListNode[U] = {
+  //def prepend[B](element: B): ListNode[B] = {
+    ListNode(element, this)
+  }
+  def head: B = h
+  def tail: Node[B] = t
+}
+case class Nil[+B]() extends Node[B] {
+  def prepend[U >: B](element: U): ListNode[U] = {
+  //def prepend[B](element: B): ListNode[B] = {
+    ListNode(element, this)
+  }
+}
+
+trait Bird
+class AfricanSwallow() extends Bird
+class EuropeanSwallow() extends Bird
+class AfricanSwallowChild() extends AfricanSwallow
+class EuropeanSwallowChild() extends EuropeanSwallow
+
+object LowerTypeBoundsTest extends App {
+  val africanSwallowList = ListNode[AfricanSwallow](new AfricanSwallow, Nil())
+  var birdList: Node[Bird] = africanSwallowList
+  birdList = birdList.prepend(new AfricanSwallowChild)
+  birdList = birdList.prepend(new EuropeanSwallow)
+  birdList = birdList.prepend(new EuropeanSwallowChild)
+  println(birdList)
+  //birdList.foreach(println)
 }
